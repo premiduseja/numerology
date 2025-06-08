7ec22a5ee9e10bfc1d8fe5bead7a742cbf7af2c6
Premi Duseja <58366224+premiduseja@users.noreply.github.com>
2025-06-08 18:03:43 -0400

    added missing files

---
+++ src/test/java/com/pd/projects/numerology/NumerologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.projects.numerology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
